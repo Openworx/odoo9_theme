12294a74b62e26e0e364920e79289c538ef2ba4f
root <root@odoo9-panel.lan>
2016-10-16 16:06:50 -0400

    Minor changes

--- v9c_backend_theme/__openerp__.py
+++ v9c_backend_theme/__openerp__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 LasLabs Inc.
+# Copyright 2016 Openworx, LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "V9c Backend Material Theme",
-    "summary": "Odoo 9.0 community Material backend theme"
+    "name": "V9c Backend Theme",
+    "summary": "Odoo 9.0 community backend theme"
                "web",
     "version": "9.0.1.0.0",
     "category": "Website",
     "website": "http://www.openworx.nl",
-    "author": "Openworx, LasLabs, Tecnativa, Odoo Community Association (OCA)",
+    "author": "Openworx",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
