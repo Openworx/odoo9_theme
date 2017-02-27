3af3776c38eb9249eb3214c112c1e7693003dbda
root <root@openworx-odoo9.lan>
2017-02-27 10:56:47 -0500

    screenshot added

--- v9c_backend_theme/__openerp__.py
+++ v9c_backend_theme/__openerp__.py
@@ -5,13 +5,16 @@
 {
     "name": "Material/United Backend Theme",
     "summary": "Odoo 9.0 community backend theme",
-    "version": "9.0.4",
+    "version": "9.0.5",
     "category": "Themes/Backend",
     "website": "http://www.openworx.nl",
 	"description": """
                 Backend theme for Odoo 9.0 community edition.
                 The app dashboard is based on the module web_responsive from LasLabs Inc and the theme on Bootstrap United
     """,
+        'images':[
+        'images/screen.png'
+        ],
     "author": "Openworx",
     "license": "LGPL-3",
     "installable": True,
